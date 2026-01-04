a5fdce0e4dc74868dd01d54d034f7e0ced3f118d
Gabriel Valentoni Guelfi <gabriel@lambdatt.com.br>
2026-01-03 23:52:52 -0300

    b

--- routes.php
+++ routes.php
@@ -12,8 +12,34 @@ class Bpm extends WebService
   {
     $this->setAntiXsrfValidation(false);
 
+    //------------- Execution Info Endpoint -------------//
+    $this->addEndpoint('GET', '/v1/execution-info/?key?', function ($key) {
+      $this->auth([
+        'BPM_EXECUTION' => 'R',
+        'BPM_STEP' => 'R',
+      ]);
+
+      // Busca as informações da execução:
+      $execInfo = $this->getDao('BPM_EXECUTION')
+        ->filter('ds_key')->equalsTo($key)
+        ->first(
+          "SELECT 
+                    exe.*, 
+                    wfl.ds_title AS workflowTitle,
+                    stp.ds_title AS stepName
+                  FROM `BPM_EXECUTION` exe
+                  JOIN `BPM_WORKFLOW` wfl ON wfl.id_bpm_workflow = exe.id_bpm_workflow
+                  JOIN `BPM_STEP` stp ON stp.id_bpm_step = exe.id_bpm_step_current
+                  WHERE exe.ds_key = ?ds_key?",
+        );
+
+      return $this->response
+        ->withStatus(200)
+        ->withData($execInfo);
+    });
+
     //------------- Available Transitions Endpoint -------------//
-    $this->addEndpoint('GET', '/v1/available-transitions/?executionKey?', function ($input) {
+    $this->addEndpoint('GET', '/v1/available-transitions/?key?', function ($key) {
       $this->auth([
         'BPM_EXECUTION' => 'R',
         'BPM_STEP' => 'R',
@@ -22,7 +48,7 @@ class Bpm extends WebService
 
       // Busca a lista de transições disponíveis com base no ID da execução:
       $availableTransitions = $this->getService('bpm/wizard')
-        ->availableTransitions($input['executionKey']);
+        ->availableTransitions($key);
 
       // Retorna a lista encontrada com Status 200:
       return $this->response
@@ -31,27 +57,27 @@ class Bpm extends WebService
     });
 
     //------------- Transition / First Step -------------//
-    $this->addEndpoint('PUT', '/v1/transition/?executionKey?/?transitionKey?', function ($input) {
+    $this->addEndpoint('PUT', '/v1/transition/?executionKey?/?transitionKey?', function ($executionKey, $transitionKey) {
       $this->auth([
         'BPM_EXECUTION' => 'U',
       ]);
 
       // Call Transition Service:
-      $this->getService('bpm/wizard')->transition($input['executionKey'], $input['transitionKey']);
+      $this->getService('bpm/wizard')->transition($executionKey, $transitionKey);
 
       // Response 204:
       return $this->response->withStatus(204);
     });
 
     //------------- BPM STEP TRACKING Record -------------//
-    $this->addEndpoint('GET', '/v1/step-tracking/?executionKey?', function ($input) {
+    $this->addEndpoint('GET', '/v1/step-tracking/?key?', function ($key) {
       $this->auth([
         'BPM_STEP' => 'R',
         'BPM_STEP_TRACKING' => 'R',
       ]);
 
       $exec = $this->getDao('BPM_EXECUTION')
-        ->filter('ds_key')->equalsTo($input['executionKey'])
+        ->filter('ds_key')->equalsTo($key)
         ->first();
 
       if (!$exec) {
@@ -68,13 +94,13 @@ class Bpm extends WebService
     });
 
     //------------- BPM Step details -------------//
-    $this->addEndpoint('GET', '/v1/step/?stepKey?', function ($input) {
+    $this->addEndpoint('GET', '/v1/step/?key?', function ($key) {
       $this->auth([
         'BPM_STEP' => 'R',
       ]);
 
       // Call Service:
-      $data = $this->getService('bpm/step')->get(['ds_key' => $input['stepKey']]);
+      $data = $this->getService('bpm/step')->get(['ds_key' => $key]);
 
       // Response 200 com o step atual;
       return $this->response
